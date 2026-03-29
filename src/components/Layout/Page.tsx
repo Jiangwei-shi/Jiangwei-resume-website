d1d4303a8a2020845771833b358efa853e1ee77b
Jiangwei-shi <a1006351432@gmail.com>
2026-03-30 01:12:24 +0800

    修改网站的图标

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -21,9 +21,8 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="/profile.ico" rel="icon" sizes="any" type="image/x-icon" />
+        <link href="/profile.ico" rel="apple-touch-icon" />
         <link href="/site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
